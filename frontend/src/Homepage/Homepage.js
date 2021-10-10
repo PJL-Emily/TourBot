65d9b3b783197a77cf7f676cd5fded163ed203ba
peiwenhuang27 <b07705025@ntu.edu.tw>
2021-10-10 12:37:33 +0800

    modfiy style

--- frontend/src/Homepage/Homepage.js
+++ frontend/src/Homepage/Homepage.js
@@ -4,15 +4,15 @@ import '../scss/main.scss';
 function Homepage() {
   return (
     <div className="App homepage">
-      <header className="App header">
-        <img src={logo} className="App logo" alt="logo" />
+        <header className="App header">
+            <img src={logo} className="App logo" alt="logo" />
 
-        <h1>TourBot</h1>
+            <h1>TourBot</h1>
 
-        <button className="App chatBtn" href="../Chatpage/Chatpage.js">
-            開始聊天
-        </button>
-      </header>
+            <button className="App chatBtn" href="../Chatpage/Chatpage.js">
+                開始聊天
+            </button>
+        </header>
     </div>
   );
 }
