adb611722ce816d598d14155b61226623a281ba2
peiwenhuang27 <b07705025@ntu.edu.tw>
2021-10-10 19:02:23 +0800

    modify modal form

--- frontend/src/Modal/ModalForm.js
+++ frontend/src/Modal/ModalForm.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import { Button, Modal, Form, Input, Radio } from 'antd';
+import { Button, Modal, Form, Radio } from 'antd';
+import '../scss/main.scss';
 
 const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
   const [form] = Form.useForm();
   return (
     <Modal
       visible={visible}
-      title="請填寫以下欄位以幫助我們推薦更好的"
-      okText="Create"
-      cancelText="Cancel"
+      title="請填寫以下欄位，以幫助我們推薦更適合的店家給您！"
+      okText="送出"
+      cancelText="取消"
       onCancel={onCancel}
       onOk={() => {
         form
@@ -16,6 +17,7 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
           .then((values) => {
             form.resetFields();
             onCreate(values);
+            // TODO: redirect to chatpage
           })
           .catch((info) => {
             console.log('Validate Failed:', info);
@@ -31,25 +33,54 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
         }}
       >
         <Form.Item
-          name="title"
-          label="Title"
+          name="gender"
+          label="您的性別"
           rules={[
             {
               required: true,
-              message: 'Please input the title of collection!',
+              message: '請填入您的性別！',
             },
           ]}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item name="description" label="Description">
-          <Input type="textarea" />
-        </Form.Item>
-        <Form.Item name="modifier" className="collection-create-form_last-form-item">
           <Radio.Group>
-            <Radio value="public">Public</Radio>
-            <Radio value="private">Private</Radio>
+            <Radio value="M">男</Radio>
+            <Radio value="F">女</Radio>
+            <Radio value="N">我不願透露</Radio>
           </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          name="age"
+          label="您的年齡層"
+          rules={[
+            {
+              required: true,
+              message: '請填入您的年齡層！',
+            },
+          ]}>
+            <Radio.Group>
+              <Radio value="1">20 歲以下</Radio>
+              <Radio value="2">20～30 歲</Radio>
+              <Radio value="3">31～40 歲</Radio>
+              <Radio value="4">41～50 歲</Radio>
+              <Radio value="5">50～60 歲</Radio>
+              <Radio value="6">60 歲以上</Radio>
+              <Radio value="N">我不願透露</Radio>
+            </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          name="purpose"
+          label="您這次旅遊的目的"
+          rules={[
+            {
+              required: true,
+              message: '請填入您的旅遊目的！',
+            },
+          ]}>
+            <Radio.Group>
+              <Radio value="sightseeing">觀光旅程</Radio>
+              <Radio value="business">商業拜訪</Radio>
+              <Radio value="N">我不願透露</Radio>
+            </Radio.Group>
         </Form.Item>
       </Form>
     </Modal>
@@ -60,6 +91,7 @@ const CollectionsPage = () => {
   const [visible, setVisible] = useState(false);
 
   const onCreate = (values) => {
+    // TODO: axios.post + then/catch
     console.log('Received values of form: ', values);
     setVisible(false);
   };
@@ -68,11 +100,14 @@ const CollectionsPage = () => {
     <div>
       <Button
         type="primary"
+        size="large"
+        shape="round"
+        className="chatBtn"
         onClick={() => {
           setVisible(true);
         }}
       >
-        New Collection
+        進入聊天室
       </Button>
       <CollectionCreateForm
         visible={visible}
