49a92fe9e1eda6e13b2e78cbc45cad20b59a3f34
pupss95315 <b07705027@ntu.edu.tw>
2021-10-24 11:22:44 +0800

    api and jwt update

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from flask.json import JSONEncoder
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from bson import json_util
 from config import Config
 import os
 from dotenv import load_dotenv
+from pymongo.errors import BulkWriteError
+
+
+class MongoJSONEncoder(JSONEncoder):
+    def default(self, obj): 
+        return json_util.default(obj)
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config["JWT_SECRET_KEY"] = "super-secret"
+# app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+# app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+app.json_encoder = MongoJSONEncoder
 Config.init_app(app)
 mongo = PyMongo(app)
 db = mongo.TourBot
+app.run()
+jwt = JWTManager(app)
 
 def tokenValid(token):
     # check validation by JWT
@@ -47,4 +62,86 @@ def exit(token):
     else:
         return "failed to exit."
 
-app.run()
+@jwt.expired_token_loader
+def expiredTokenCallback(jwt_header, jwt_payload):
+    return jsonify(message="token expired."), 401
+
+@app.route('/protected', methods=['GET', 'POST'])
+@jwt_required()
+def protected(): 
+    identity = get_jwt_identity()
+    return jsonify(msg='ok'), 200
+
+@app.route('/submitUserInfo', methods=['POST'])
+def submitUserInfo():
+    try:
+        userInfo = request.get_json()
+        user_dict = {
+            'purpose':userInfo['purpose']
+            # 'gender':userInfo['gender'],
+            # 'age':userInfo['age']
+        }
+        new_user = db.users.insert_one(user_dict).inserted_id
+
+        # create jwt token
+        accesstoken = create_access_token(identity=new_user)
+        return jsonify({'message':'ok', 'data':{'accesstoken': accesstoken}}), 200
+        # return jsonify({'message':'ok', "user": new_user}), 200
+    except BulkWriteError as e:
+        return jsonify({'message':'Failed to submit user info.'}), 400
+
+@app.route('/getUserState', methods=['GET'])
+@jwt_required()
+def getUserState():
+    try:
+        token = request.get_json()['token']
+        user = db.users.find({"token": token})
+        state = db.states.find({"user_id": user._id.str})
+        return jsonify({'message':'ok', 'data':state}), 200
+    except BulkWriteError as e:
+        return jsonify({'message':'Failed to get user state.'}), 400
+
+@app.route('/getHotelInfo', methods=['GET'])
+@jwt_required()
+def getHotelInfo():
+    try:
+        # token = request.get_json()['token']
+        # user = db.users.findOne({"token": token})
+        # state = db.states.findOne({"user_id": user._id.str})
+        # hotel = db.hotels.findOne({"name": state.hotel})
+
+        #test
+        hotel = request.get_json(force=True)['hotel']
+        hotel = db.hotel.find_one({"name": hotel}, {'_id': False})
+        return jsonify({'message':'ok', "hotel": hotel}), 200
+    except BulkWriteError as e:
+        return jsonify({'message':'Failed to get hotel info.'}), 400
+
+@app.route('/getSiteInfo', methods=['GET'])
+@jwt_required()
+def getSiteInfo():
+    try:
+        user = db.users.findOne({"token": token})
+        state = db.states.findOne({"user_id": user._id.str})
+        site = db.sites.findOne({"name": state.site})
+        return jsonify({'message':'ok',  'data':site}), 200
+    except BulkWriteError as e:
+        return jsonify({'message':'Failed to get site info.'}), 400
+
+@app.route('/getRestInfo', methods=['GET'])
+@jwt_required()
+def getRestInfo():
+    try:
+        user = db.users.findOne({"token": token})
+        state = db.states.findOne({"user_id": user._id.str})
+        rest = db.rests.findOne({"name": state.rest}), 200
+        return jsonify({'message':'ok',  'data':rest})
+    except BulkWriteError as e:
+        return jsonify({'message':'Failed to get rest info.'}), 400
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
